84fa6df1ba125c705701898109f292d2acb89bf4
deoramadanu <deo121005@gmail.com>
2025-04-28 19:24:16 +0700

    membuat input data

--- IntroduceOOP.cpp
+++ IntroduceOOP.cpp
@@ -11,6 +11,15 @@ float diameter;
 void displaydata () {
     cout << "my gravity is" << gravity << endl;
 }
+void inputdata(){
+    cout << "input nama : ";
+    cin >> name;
+    cout << "ukuranku : ";
+    cin >> diameter;
+    cout << "my gravity is :";
+    cin >> gravity;
 
+
+}
 
 }
